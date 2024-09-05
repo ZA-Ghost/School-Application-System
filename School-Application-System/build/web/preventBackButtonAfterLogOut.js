f6d1756789237cb5774a393ae20568a7c742f60e
ZA_Ghost <119295603+ZA-Ghost@users.noreply.github.com>
2024-09-05 19:51:04 +0200

    Delete School-Application-System directory

--- School-Application-System/build/web/preventBackButtonAfterLogOut.js
+++
@@ -1,5 +0,0 @@
-// Check if the page was loaded from the cache
-if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_BACK_FORWARD) {
-    // Redirect to the login page or any other desired page
-    window.location.href = "index.html";
-}
